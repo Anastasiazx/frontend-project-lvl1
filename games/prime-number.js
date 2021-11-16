eb518ad4d6151b703f36e2521d138f0a89d57fda
Anastasiia Piatiletova <Nasten4ik1996@mail.ru>
2021-11-16 17:26:44 +0700

    Add brain-prime.js

---
+++ games/prime-number.js
@@ -0,0 +1,29 @@
+const correctAnswer = (number) => {
+  let count = 1;
+
+  if (number === 0) {
+    return 0;
+  }
+
+  for (let i = 2; i <= number; i += 1) {
+    if (number % i === 0) {
+      count += 1;
+    }
+  }
+  if (count !== 2) {
+    return false;
+  }
+  return true;
+};
+
+export const rules = () => {
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+};
+
+export const findPrimeNumber = () => {
+  const number = Math.floor(Math.random() * 100);
+  const array = [`Question: ${number}`, 'answer'];
+  const result = (correctAnswer(number)) ? 'yes' : 'no';
+  array[1] = result;
+  return array;
+};
