ed8b0b41d35337581f98c7ec724d3e2fdb4393a3
Anastasiia Piatiletova <Nasten4ik1996@mail.ru>
2021-11-18 13:50:27 +0700

    Update brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-import play from '../src/index.js';
-import * as parity from '../src/games/parity-check.js';
+import runParityCheck from '../src/games/parity-check.js';
 
-play(parity.rules, parity.parityCheck);
+runParityCheck();
--- src/games/parity-check.js
+++ src/games/parity-check.js
@@ -1,13 +1,15 @@
 import getRandomIntInclusive from '../random-numbers.js';
+import runEngine from '../index.js';
 
-export const rules = () => {
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-};
+const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-export const parityCheck = () => {
+const parityCheck = () => {
   const number = getRandomIntInclusive(0, 100);
   const array = [`Question: ${number}`, 'answer'];
   const result = (number % 2 === 0) ? 'yes' : 'no';
   array[1] = result;
   return array;
 };
+
+const runParityCheck = () => runEngine(rules, parityCheck);
+export default runParityCheck;
