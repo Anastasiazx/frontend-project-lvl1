93b88b8af35c56f5112c802237e362f50f597a28
Anastasiia Piatiletova <Nasten4ik1996@mail.ru>
2021-11-18 13:12:24 +0700

    Add file random-numbers.js

---
+++ src/random-numbers.js
@@ -0,0 +1,6 @@
+const getRandomIntInclusive = (min, max) => {
+  const randomNumber = Math.random() * (max - min + 1) + min;
+  return Math.floor(randomNumber);
+};
+
+export default getRandomIntInclusive;
