0f31c069e3f5ecf180399f5dabfc51d97a2ab730
Anastasiia Piatiletova <Nasten4ik1996@mail.ru>
2021-11-25 13:06:10 +0700

    Update brain-calc.js

--- src/games/calculator.js
+++ src/games/calculator.js
@@ -20,7 +20,7 @@ const calculate = () => {
   const number1 = getRandomIntInclusive(0, 100);
   const number2 = getRandomIntInclusive(0, 100);
   const operators = ['+', '-', '*'];
-  const operator = operators[getRandomIntInclusive(0, operators.length)];
+  const operator = operators[getRandomIntInclusive(0, operators.length - 1)];
   const question = `${number1} ${operator} ${number2}`;
   const answer = String(correctAnswer(number1, number2, operator));
   return [question, answer];
